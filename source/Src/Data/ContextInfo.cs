f7f51c70aa724af654762cb8ad7ba84b00f05071
Dmytro Morozov <mdmsua@icloud.com>
2015-03-02 20:40:11 +0200

    Set method is now public. Claims as value source.

--- source/Src/Data/ContextInfo.cs
+++ source/Src/Data/ContextInfo.cs
@@ -9,9 +9,10 @@
 
 namespace Microsoft.Practices.EnterpriseLibrary.Data
 {
-    using System;
     using System.Data;
     using System.Data.Common;
+    using System.Security.Claims;
+    using System.Threading;
 
     /// <summary>
     ///     The context info.
@@ -24,82 +25,16 @@ namespace Microsoft.Practices.EnterpriseLibrary.Data
         private const string CommandText = "log.SaveContext";
 
         /// <summary>
-        /// The user.
-        /// </summary>
-        [ThreadStatic]
-        private static string user;
-
-        /// <summary>
-        /// The customer.
-        /// </summary>
-        [ThreadStatic]
-        private static string customer;
-
-        /// <summary>
-        /// The role.
-        /// </summary>
-        [ThreadStatic]
-        private static string role;
-
-        /// <summary>
-        /// Gets or sets the user.
-        /// </summary>
-        public static string User
-        {
-            get
-            {
-                return user;
-            }
-
-            set
-            {
-                user = value;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the role.
-        /// </summary>
-        public static string Role
-        {
-            get
-            {
-                return role;
-            }
-
-            set
-            {
-                role = value;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the customer.
-        /// </summary>
-        public static string Customer
-        {
-            get
-            {
-                return customer;
-            }
-
-            set
-            {
-                customer = value;
-            }
-        }
-
-        /// <summary>
         /// Sets context info and executes stored procedure on the connection.
         /// </summary>
         /// <param name="connection">
         /// The connection.
         /// </param>
-        internal static void Set(DbConnection connection)
+        public static void Set(DbConnection connection)
         {
             using (var command = connection.CreateCommand())
             {
-                SetParameters(command);
+                AssignParameters(command);
                 command.CommandText = CommandText;
                 command.CommandType = CommandType.StoredProcedure;
                 command.Connection = connection;
@@ -109,36 +44,64 @@ namespace Microsoft.Practices.EnterpriseLibrary.Data
         }
 
         /// <summary>
-        /// The set parameters.
+        /// Assigns parameters to a command.
         /// </summary>
         /// <param name="command">
         /// The command.
         /// </param>
-        private static void SetParameters(DbCommand command)
+        private static void AssignParameters(DbCommand command)
         {
+            var claimsIdentity = Thread.CurrentPrincipal.Identity as ClaimsIdentity;
+            if (claimsIdentity == null)
+            {
+                return;
+            }
+
+            var user = FindFirstValue(claimsIdentity, claimsIdentity.NameClaimType);
+            var role = FindFirstValue(claimsIdentity, claimsIdentity.RoleClaimType);
+            var system = FindFirstValue(claimsIdentity, ClaimTypes.System);
+
             var userParameter = command.CreateParameter();
             userParameter.DbType = DbType.String;
             userParameter.Direction = ParameterDirection.Input;
-            userParameter.IsNullable = false;
+            userParameter.IsNullable = true;
             userParameter.ParameterName = "@User";
             userParameter.Value = user;
             command.Parameters.Add(userParameter);
 
-            var customerParameter = command.CreateParameter();
-            customerParameter.DbType = DbType.String;
-            customerParameter.Direction = ParameterDirection.Input;
-            customerParameter.IsNullable = false;
-            customerParameter.ParameterName = "@Customer";
-            customerParameter.Value = customer;
-            command.Parameters.Add(customerParameter);
-
             var roleParameter = command.CreateParameter();
             roleParameter.DbType = DbType.String;
             roleParameter.Direction = ParameterDirection.Input;
-            roleParameter.IsNullable = false;
+            roleParameter.IsNullable = true;
             roleParameter.ParameterName = "@Role";
             roleParameter.Value = role;
             command.Parameters.Add(roleParameter);
+
+            var customerParameter = command.CreateParameter();
+            customerParameter.DbType = DbType.String;
+            customerParameter.Direction = ParameterDirection.Input;
+            customerParameter.IsNullable = true;
+            customerParameter.ParameterName = "@Customer";
+            customerParameter.Value = system;
+            command.Parameters.Add(customerParameter);
+        }
+
+        /// <summary>
+        /// Finds first value.
+        /// </summary>
+        /// <param name="claimsIdentity">
+        /// The claims identity.
+        /// </param>
+        /// <param name="claimType">
+        /// The claim type.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        private static string FindFirstValue(ClaimsIdentity claimsIdentity, string claimType)
+        {
+            var claim = claimsIdentity.FindFirst(claimType);
+            return claim == null ? null : claim.Value;
         }
     }
 }
--- source/Src/Data/Properties/AssemblyInfo.cs
+++ source/Src/Data/Properties/AssemblyInfo.cs
@@ -11,9 +11,9 @@ using Microsoft.Practices.EnterpriseLibrary.Data.Configuration;
 [assembly: ReliabilityContract(Consistency.WillNotCorruptState, Cer.None)]
 [assembly: AssemblyTitle("Enterprise Library Data Access Application Block")]
 [assembly: AssemblyDescription("Enterprise Library Data Access Application Block")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0-release")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0-release")]
 
 [assembly: SecurityTransparent]
 
